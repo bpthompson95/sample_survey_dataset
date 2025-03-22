c8c72d49e392209efbdfc80075c8cf2a5b257210
Ben <54786519+bpthompson95@users.noreply.github.com>
2025-03-22 15:23:54 -0400

    Update sample_dataset_driver.R

--- sample_dataset_driver.R
+++ sample_dataset_driver.R
@@ -1,6 +1,6 @@
 # Function for generating both binary and ordinal/Likert-type data...
-# -- Required inputs are the stratification variable (strata), the variable codes, the desired mean values per stratum, and the standard deviation per stratum.
-# -- Allows for specification of variable means and standard deviations within strata.
+# -- Required inputs are the stratification variable (strata), an array of the variable's codes, an array of the desired mean values per stratum, and an array of the standard deviations per stratum.
+# -- Allows for specification of response means and dispersion within each stratum.
 generate_data = function(strata, codes, mean_values, sd) {
   question_data <- sapply(strata, function(s) {
     sample(codes, 1, replace = TRUE, prob = dnorm(codes, mean = mean_values[s], sd = sd[s]))
